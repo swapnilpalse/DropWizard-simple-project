4ec57d5ca3c3d92c357f3852b86a9984e9a0fdb1
Palse,Swapnil <swapnil.palse@gmail.com>
2018-10-02 12:39:02 -0500

    Added the resource file for the project which is the end point of the service

---
+++ src/main/java/com/dropwizard/learning/hellowrold/resources/HelloWorldResources.java
@@ -0,0 +1,34 @@
+package com.dropwizard.learning.hellowrold.resources;
+
+import com.codahale.metrics.annotation.Timed;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import java.util.concurrent.atomic.AtomicLong;
+import com.dropwizard.learning.hellowrold.api.Saying;
+import com.google.common.base.Optional;
+
+
+@Path("/hello-world")
+@Produces(MediaType.APPLICATION_JSON)
+public class HelloWorldResources {
+    private final String template;
+    private final String defaultName;
+    private final AtomicLong counter;
+
+    public HelloWorldResources(String template, String defaultName, AtomicLong counter) {
+        this.template = template;
+        this.defaultName = defaultName;
+        this.counter = counter;
+    }
+
+    @GET
+    @Timed
+    public Saying sayHello(@QueryParam("name") Optional<String> name) {
+        final String value = String.format(template, name.or(defaultName));
+        return new Saying(counter.incrementAndGet(), value);
+    }
+}
